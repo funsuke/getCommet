82e14d1f4ce1369989e279d26472e83817481195
funsuke <funsuke0327@yahoo.co.jp>
2025-08-05 23:01:04 +0900

    first commit

---
+++ src/main.ts
@@ -0,0 +1,77 @@
+// テンプレート
+// akashic init -t typescript
+// gの定義
+// https://akashic-games.github.io/guide/common-pitfalls.html
+// npm install -DE @akashic/akashic-engine
+// Zipファイル(上書き)
+// akashic export zip --output game.zip --nicolive -f
+// マルチのテストは
+// akashic serve -s nicolive
+
+function main(param: g.GameMainParameterObject): void {
+	const scene = new g.Scene({ game: g.game });
+	// -------------------------------------------------------------
+	// シーン押下時(マルチテスト)
+	// -------------------------------------------------------------
+	scene.onPointDownCapture.add((ev: g.PointDownEvent) => {
+		const rect = new g.FilledRect({
+			scene: scene,
+			cssColor: "red",
+			x: ev.point.x,
+			y: ev.point.y,
+			width: 50,
+			height: 50,
+		});
+		scene.append(rect);
+	});
+	let cntRow = 0;
+	// -------------------------------------------------------------
+	// コメントの受信を行うためのイベントリスナーを追加
+	// -------------------------------------------------------------
+	scene.onMessage.add((ev: g.MessageEvent) => {
+		if (ev.data && ev.data.type === "namagame:comment") {
+			const comments = ev.data.comments;
+			for (const c of comments) {
+				const label = new g.Label({
+					scene: scene,
+					text: c.isAnonymous ? " (匿名)" : "" + c.comment,
+					font: new g.DynamicFont({
+						game: g.game,
+						fontFamily: "sans-serif",
+						size: 16
+					}),
+					x: 10,
+					y: 50 + 20 * cntRow + 20 * comments.indexOf(c),
+					width: 300,
+					height: 20
+				});
+				scene.append(label);
+			}
+			cntRow += comments.length;
+		}
+	});
+	// -------------------------------------------------------------
+	// 読込時処理
+	// -------------------------------------------------------------
+	scene.onLoad.add(() => {
+		const label = new g.Label({
+			scene: scene,
+			text: "マルチゲームのテスト",
+			font: new g.DynamicFont({
+				game: g.game,
+				fontFamily: "sans-serif",
+				size: 24
+			}),
+			x: 10,
+			y: 10,
+			width: 300,
+			height: 50
+		});
+		scene.append(label);
+		// コメントの受信を開始
+		g.game.external.namagameComment?.start();
+	});
+	g.game.pushScene(scene);
+}
+
+export = main;
